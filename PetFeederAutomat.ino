89f31308686f8d3e810756211990e9605c356397
Badara Denisa <denisabadara2003@gmail.com>
2025-05-23 10:21:55 +0300

    Add GPIO and PWM setup for servo control

--- PetFeederAutomat.ino
+++ PetFeederAutomat.ino
@@ -5,12 +5,35 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 DS3231 rtc(SDA, SCL);
 Time t;
 
+#define BTN_PORTII PD2  // INT0
+#define BTN_ORA    PD3  // INT1
+#define BTN_MANUAL PD5  
+#define SERVO_PIN  PB1  // OC1A = pin 9
+
 int portions_crt = 1;
 int interval = 1;
 
 int next_feed_hour = 19;
 int next_feed_minute = 24;
 int next_feed_seconds = 01;
+
+void setup_gpio() {
+  DDRD &= ~((1 << BTN_PORTII) | (1 << BTN_ORA) | (1 << BTN_MANUAL)); 
+  PORTD |= (1 << BTN_PORTII) | (1 << BTN_ORA) | (1 << BTN_MANUAL);   
+}
+
+void setup_pwm_servo() {
+  DDRB |= (1 << SERVO_PIN); 
+
+  TCCR1A = (1 << COM1A1) | (1 << WGM11);              // Fast PWM
+  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11);  // Prescaler 8
+  ICR1 = 39999; // 20ms (50Hz) PWM
+}
+
+void set_servo_angle(int angle) {
+  uint16_t pulse_us = map(angle, 0, 180, 1000, 2000);
+  OCR1A = pulse_us * 2; 
+}
 
 void update_lcd() {
   lcd.setCursor(0, 0);
@@ -24,8 +47,11 @@ void update_lcd() {
 void setup() {
   lcd.init();
   lcd.backlight();
+  setup_gpio();
+  setup_pwm_servo();
   rtc.begin();
   Serial.begin(9600);
+
 }
 
 void loop() {
